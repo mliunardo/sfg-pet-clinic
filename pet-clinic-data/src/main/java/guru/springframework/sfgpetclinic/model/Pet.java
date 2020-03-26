e436ed8855ff10a301b1512e4f6b445a978b51db
mliunardo <mliunardo@gmail.com>
2020-03-26 12:15:20 +0800

    Refactoring to multimodule maven project. closes #1

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,5 +1,4 @@
 package guru.springframework.sfgpetclinic.model;
-
 import java.time.LocalDate;
 
 public class Pet {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,5 +1,4 @@
 package guru.springframework.sfgpetclinic.model;
-
 public class PetType {
 
 	private String name;
